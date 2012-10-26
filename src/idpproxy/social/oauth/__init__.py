61941294f6f7172610d107744fa86a8be5eec3b9
Roland Hedberg <roland.hedberg@adm.umu.se>
2012-10-26 15:30:10 +0200

    Made LinkenIn work .. to some extent still missing the 'id' attribute when I'm testing with my user.

--- src/idpproxy/social/oauth/__init__.py
+++ src/idpproxy/social/oauth/__init__.py
@@ -24,7 +24,14 @@ class OAuth(Social):
         session = server_env["CACHE"][sid]
 
         client = oauth.Client(self.consumer)
-        resp, content = client.request(self.extra["request_token_url"], "GET")
+        if self.extra["scope"]:
+            url = "%s?scope=%s" % (self.extra["request_token_url"],
+                                  "+".join(self.extra["scope"]))
+        else:
+            url = self.extra["request_token_url"]
+
+        logger.debug("Request_token url: %s" % url)
+        resp, content = client.request(url, "GET")
         if server_env["DEBUG"]:
             logger.info("Client resp: %s" % resp)
             logger.info("Client content: %s" % content)
@@ -45,8 +52,10 @@ class OAuth(Social):
             dig = server_env["CACHE"].digest(sid)
             session["sid_digest"] = dig
 
-        callback_url = "%s%s/%s" % (server_env["base_url"], self.extra["name"],
+        callback_url = "%s%s/%s" % (server_env["base_url"],
+                                    self.social_endpoint,
                                     session["sid_digest"])
+        logger.debug("callback_url: %s" % callback_url)
         # Step 2: Redirect to the provider.
 
         args = {
@@ -58,9 +67,17 @@ class OAuth(Social):
 
         url = "%s?%s" % (self.extra["authorization_endpoint"],
                          urllib.urlencode(args))
+        logger.debug("redirect to: %s" % url)
         start_response("302 Found", [("Location", url), cookie])
         return []
 
+    def get_profile(self, info_set):
+        """
+        default is you're not going to get anymore
+        :param info_set:
+        :return:
+        """
+        return {"status": "200"}, info_set
 
     #noinspection PyUnusedLocal
     def phaseN(self, environ, info, server_env, sid):
@@ -71,31 +88,51 @@ class OAuth(Social):
         You should store this access token somewhere safe, like a database, for
         future use."""
 
-        logger.info("response_oauth_token: %s" % info["oauth_token"][0])
+        logger.debug("[phaseN] info: %s" % info)
         session = server_env["CACHE"][sid]
 
         try:
-            key = info["oauth_token"][0]
+            response_oauth_token = info["oauth_token"][0]
         except KeyError:
             session["authentication"] = "FAILED"
             return False, "Access denied", session
 
-        token = session[key]
+        logger.info("response_oauth_token: %s" % response_oauth_token)
+
+        token = session[response_oauth_token]
 
         #token.set_verifier(oauth_verifier)
         client = oauth.Client(self.consumer, token)
 
-        resp, content = client.request(self.extra["token_endpoint"], "POST")
+        if "oauth_verifier" in info:
+            _body = "oauth_verifier=%s" % info["oauth_verifier"][0]
+        else:
+            _body = ""
+
+        resp, content = client.request(self.extra["token_endpoint"], "POST",
+                                       body=_body)
         if resp['status'] != '200':
+            logger.error("<token_endpoint> response: %s, content: %s" % (resp,
+                                                                         content))
             return False, "Invalid response %s." % resp['status'], session
 
-        profile = dict(parse_qs(content))
         if server_env["DEBUG"]:
-            logger.info("[%s phase2] profile: %s" % (self.extra["name"],
-                                                     profile))
+            logger.info("[%s phase2] content: %s" % (self.name, content))
+
+        info_set = dict(parse_qs(content))
+
+        resp, profile = self.get_profile(info_set)
+
+        if resp['status'] != '200':
+            logger.error("<token_endpoint> response: %s, content: %s" % (resp,
+                                                                         content))
+            return False, "Invalid response %s." % resp['status'], session
+
+        if server_env["DEBUG"]:
+            logger.info("[%s phase2] profile: %s" % (self.name, profile))
 
         session["permanent_id"] = profile["user_id"][0]
-        session["service"] = self.extra["name"]
+        session["service"] = self.name
         session["service_info"] = profile
         session["authn_auth"] = self.authenticating_authority
         session["authentication"] = "OK"
