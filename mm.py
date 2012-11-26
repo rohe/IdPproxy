7603c23215effc2af74ead70805464e56a1a50a2
Roland Hedberg <roland.hedberg@adm.umu.se>
2012-11-26 10:16:07 +0100

    Change in metadata class name. Allow for better control over what is produced.

--- mm.py
+++ mm.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import argparse
+
 from saml2.sigver import read_cert_from_file
 from saml2.time_util import in_a_while
 
@@ -19,7 +20,7 @@ def do_key_descriptor(cert):
     return KeyDescriptor(
         key_info = ds.KeyInfo(
             x509_data=ds.X509Data(
-                x509_certificate=ds.X509DataType_X509Certificate(text=cert)
+                x509_certificate=ds.X509Certificate(text=cert)
             )
         )
     )
@@ -36,7 +37,8 @@ def entity_desc(loc, key_descriptor=None, eid=None, validity=None, cache=None,
 
     return ei
 
-def entities_desc(service, ename, base, cert_file=None, validity="", cache=""):
+def entities_desc(service, ename, base, cert_file=None, validity="", cache="",
+                  social=None):
     ed = []
     if cert_file:
         _cert = read_cert_from_file(cert_file, "pem")
@@ -45,27 +47,46 @@ def entities_desc(service, ename, base, cert_file=None, validity="", cache=""):
         key_descriptor = None
 
     for name, desc in service.items():
-        loc = "%s/%s" % (base, desc["saml_endpoint"])
-        eid = "%s/%s" % (base, name)
-        ed.append(entity_desc(loc, key_descriptor, eid))
-
+        if social is None or name in social:
+            loc = "%s/%s" % (base, desc["saml_endpoint"])
+            #eid = "%s/%s" % (base, name)
+            ed.append(entity_desc(loc, key_descriptor, loc))
 
     return EntitiesDescriptor(name=ename, entity_descriptor=ed,
                               valid_until = in_a_while(hours=validity),
                               cache_duration=cache)
 
 if __name__ == "__main__":
-    import sys
-
     from config import idp_proxy_conf as ipc
-    cnf = __import__(sys.argv[1])
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', dest="cnf",
+                              help="The idp configuration file")
+    parser.add_argument('-v', dest="validity",
+                            help="Indicates the expiration time of the metadata",
+                            type=int, default=6)
+    parser.add_argument('-d', dest="duration",
+                        help="Indicates the maximum length of time a consumer should cache the metadata",
+                        type=int)
+    parser.add_argument("soc", nargs="*", help="Which social services to include")
+
+    args = parser.parse_args()
+
+    cnf = __import__(args.cnf)
 
     if cnf.BASE.endswith("/"):
         _base = cnf.BASE[:-1]
     else:
         _base = cnf.BASE[:]
 
+    kwargs = {}
+    if args.soc:
+        kwargs["social"] = args.soc
+    if args.duration:
+        kwargs["cache"] = args.duration
+
     ed = entities_desc(ipc.SERVICE, "%s/md/idpproxy-1.0.xml" % _base,
-                       _base, cert_file=cnf.CONFIG["cert_file"], validity=1)
+                       _base, cert_file=cnf.CONFIG["cert_file"],
+                       validity=args.validity, **kwargs)
 
     print ed
