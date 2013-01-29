09f5f72ec91d16bf58fcce2b9d6cd2ac7f715d1b
Roland Hedberg <roland.hedberg@adm.umu.se>
2013-01-29 16:20:04 +0100

    Renamed so not to overwrite.

--- idp_conf.example.py
+++ idp_conf.example.py
@@ -16,16 +16,16 @@ CONFIG = {
             "endpoints" : {
                 "single_sign_on_service" : [(BASE, BINDING_HTTP_REDIRECT)],
                 "single_logout_service" : [(BASE+"logout", BINDING_SOAP)],
-            },
+                },
             "policy": {
                 "default": {
                     "lifetime": {"minutes": 240},
                     "attribute_restrictions": None, # means all I have
                     "name_form": NAME_FORMAT_URI
                 },
-            },
+                },
             "subject_data": "./idp.subject.db",
-        }
+            }
     },
     "debug" : 1,
     "key_file" : "pki/mykey.pem",
@@ -33,12 +33,12 @@ CONFIG = {
     "xmlsec_binary" : "/opt/local/bin/xmlsec1",
     "metadata" : {
         "local": ["test/sp/sp.xml"],
-    },
+        },
     "organization": {
         "display_name": "Rolands Social proxy",
         "name": "Rolands Social proxy",
         "url":"http://www.example.com/roland",
-    },
+        },
     # This database holds the map between a subjects local identifier and
     # the identifier returned to a SP
     "attribute_map_dir" : "./attributemaps",
@@ -48,7 +48,7 @@ CONFIG = {
             "filename": "idp_proxy.log",
             "maxBytes": 100000,
             "backupCount": 5,
-        },
+            },
         "loglevel": "debug",
         #"format": "%(asctime)s %(name)s:%(func)s %(levelname)s %(message)s",
         "format": "%(asctime)s %(name)s %(levelname)s %(message)s"
