5b4ae60efbb70621c9a4fb9067ee7009cb1c8585
Roland Hedberg <roland.hedberg@adm.umu.se>
2013-04-10 09:21:10 +0200

    To make the data files be placed where they should be.

--- setup.py
+++ setup.py
@@ -2,24 +2,39 @@
 from setuptools import setup, find_packages
 __author__ = 'rolandh'
 
-setup(
-        name="idpproxy",
-        version="0.2.1",
-        description="Proxy between social services and SAML2",
-        author="Roland Hedberg",
-        author_email="roland.hedberg@adm.umu.se",
-        license="Apache 2.0",
-        packages=find_packages("src"),
-        package_dir = {"":"src"},
-        classifiers = ["Development Status :: 4 - Beta",
-                "License :: OSI Approved :: Apache Software License",
-                "Topic :: Software Development :: Libraries :: Python Modules"],
+PRE = "src/idpproxy/metadata/files"
+HTDOCS = "%s/mako/htdocs" % PRE
+TEMPL = "%s/mako/templates" % PRE
+STAT = "%s/static" % PRE
+TARGET = "idpproxy/metadata/files"
 
-        install_requires=[
-                'pysaml2',
-                'mako',
-                'pycrypto',
-                'oic',
-        ],
-        zip_safe=False,
+setup(
+    name="idpproxy",
+    version="0.2.1",
+    description="Proxy between social services and SAML2",
+    author="Roland Hedberg",
+    author_email="roland.hedberg@adm.umu.se",
+    license="Apache 2.0",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    install_requires=[
+        'pysaml2',
+        'mako',
+        'pycrypto',
+        'oic',
+    ],
+    zip_safe=False,
+    data_files=[
+        ("%s/mako/htdocs" % TARGET, ["%s/metadata.mako" % HTDOCS,
+                                     "%s/metadatasave.mako" % HTDOCS]),
+        ("%s/mako/templates" % TARGET, ["%s/root.mako" % TEMPL]),
+        ("%s/static" % TARGET, ["%s/style.css" % STAT,
+                                "%s/unknown.html" % STAT,
+                                "%s/unknownError.html" % STAT])
+    ]
 )
